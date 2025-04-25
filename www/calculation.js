d768633767cbe51146cb64d30ae05862d737f7c1
jhinx38 <hakimzack42@gmail.com>
2025-04-25 15:25:53 +0800

    Test upload pic on mobile

--- www/calculation.js
+++ www/calculation.js
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA7ePIVMUiVFM5WEwXXrdg-_qR7ras1VPw",
   authDomain: "mygaji.firebaseapp.com",
   projectId: "mygaji-7212c",
-  storageBucket: "mygaji.appspot.com",
+  storageBucket: "mygaji-7212c.firebasestorage.app",
   messagingSenderId: "745825441000",
   appId: "1:745825441000:android:89d1dabda0ee140dfbe55d"
 };
