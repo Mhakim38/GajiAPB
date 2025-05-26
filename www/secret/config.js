afac9442fbc012e5cb56487f86d8d8e83e6fedf0
jhinx38 <hakimzack42@gmail.com>
2025-05-26 09:13:13 +0800

    seperate the API token

---
+++ www/secret/config.js
@@ -0,0 +1,9 @@
+// Firebase configuration
+export const firebaseConfig = {
+  apiKey: "AIzaSyA7ePIVMUiVFM5WEwXXrdg-_qR7ras1VPw",
+  authDomain: "mygaji.firebaseapp.com",
+  projectId: "mygaji-7212c",
+  storageBucket: "mygaji-7212c.firebasestorage.app",
+  messagingSenderId: "745825441000",
+  appId: "1:745825441000:android:89d1dabda0ee140dfbe55d"
+};
